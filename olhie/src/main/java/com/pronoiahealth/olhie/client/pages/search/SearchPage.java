e11672897120de4e698fcb77878e3b7f05122c46
John DeStefano <jjdestef3@gmail.com>
2013-11-29 15:23:53 -0500

    Documentation for the search process.

--- olhie/src/main/java/com/pronoiahealth/olhie/client/pages/search/SearchPage.java
+++ olhie/src/main/java/com/pronoiahealth/olhie/client/pages/search/SearchPage.java
@@ -14,7 +14,6 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
 
-import org.apache.deltaspike.core.api.config.ConfigProperty;
 import org.jboss.errai.ui.nav.client.local.Page;
 import org.jboss.errai.ui.nav.client.local.PageHidden;
 
@@ -32,6 +31,34 @@ import com.pronoiahealth.olhie.client.shared.events.local.SearchPageLoadedEvent;
  * Responsibilities:<br/>
  * 1. Shows Search page<br/>
  * 2. Fires the SearchPageLoadedEvent<br/>
+ * 
+ * <p>
+ * How this works<br/>
+ * The user selects this page from the left hand navigation menu. They then
+ * enter a search string in the find box and select the find button. The find
+ * box and button are part of the SearchComponent widget. Pressing the find
+ * button causes a BookSearchEvent to be fire by the SearchComponent. This event
+ * is handled by the BookSearchService. The method in that class that handles
+ * this event will return a BookSearchResponseEvent containing BookDisplays upto
+ * the page search size (defined in the detaspike configuration file
+ * (/META-INF/apache-deltaspike.properties). It will also save the state of the
+ * search in the session scoped SearchResultHolder. The holder will save the
+ * necessary data to respond to future SearchPageNavigationRequestEvent. The
+ * BookSearchResponseEvent is also listened for by the SearchPagerWidget. When
+ * the widget recieves this event it will fire a
+ * SearchPageNavigationRequestEvent with and action of UPDATE_STATE. The
+ * receiving method in the BookSearchService will fire a
+ * SearchPageNavigationResponseEvent which will be handled by the
+ * SearchResultsComponent and the SearchPagerWidget. Because this event will
+ * have the original action of UPDATE_STATE the SearchResultsComponent will do
+ * nothing with it. However the SearchPagerWidget will update the state of the
+ * pager buttons and results display. When buttons are selected on the
+ * SearchPagerWidget component the SearchPageNavigationRequestEvent will be
+ * fired and handled by the BookSearchService. Depending on what button is
+ * pressed the appropriate response will be returned in the
+ * SearchPageNavigationResponseEvent. As above this event is observed for by the
+ * SearchPagerWidget and the SearchResultsComponent.
+ * </p>
  * 
  * @author John DeStefano
  * @version 1.0
